06b34582f5d00368837ffa902b4fee684d70cdf3
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-25 18:09:36 +0530

    Add Intro component with DateCounter

---
+++ app/intro/page.js
@@ -0,0 +1,9 @@
+import DateCounter from "@/components/DateCounter";
+
+export default function Intro() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gradient-to-b from-pink-200 to-purple-300 text-black">
+      <DateCounter />
+    </div>
+  );
+}
