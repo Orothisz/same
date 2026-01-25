1e313d25f82e926dc709bed7d8f32a61051f939b
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-25 18:10:14 +0530

    Create page.js

---
+++ app/memories/page.js
@@ -0,0 +1,11 @@
+import PhotoCard from "@/components/PhotoCard";
+
+export default function Memories() {
+  return (
+    <div className="min-h-screen p-10 grid grid-cols-1 md:grid-cols-3 gap-6 bg-[#111]">
+      {[...Array(6)].map((_, i) => (
+        <PhotoCard key={i} index={i + 1} />
+      ))}
+    </div>
+  );
+}
