9bbfaf87915b93207d210a10420333249a8b65f0
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-25 18:08:56 +0530

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Sameha Land 💗",
+  description: "100 Days of Love",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
