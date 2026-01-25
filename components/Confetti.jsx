8b0c1e982b326b326d80b5a0f5ccf497df0bf89a
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-25 18:13:05 +0530

    Add Confetti component for celebration visuals

---
+++ components/Confetti.jsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Confetti() {
+  return (
+    <>
+      <img src="/gifs/celebration/confetti.gif" className="absolute top-0" />
+      <img src="/gifs/celebration/dance-1.gif" className="w-32 mt-6" />
+    </>
+  );
+}
