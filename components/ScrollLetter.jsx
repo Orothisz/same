19d2d33dc7848c404514989e9a9df2eae4d737e3
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-25 18:12:23 +0530

    Add ScrollLetter component to display letter text

---
+++ components/ScrollLetter.jsx
@@ -0,0 +1,13 @@
+import { letterText } from "@/data/letterText";
+
+export default function ScrollLetter() {
+  return (
+    <div className="min-h-screen p-10 bg-[#f6f1e9] text-black">
+      <div className="max-w-2xl mx-auto bg-white p-8 rounded-xl shadow-xl">
+        <p className="font-handwritten text-xl whitespace-pre-line">
+          {letterText}
+        </p>
+      </div>
+    </div>
+  );
+}
