08b0d8dec7edae78cf510c3a01d0f3e0c388159a
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-28 17:17:15 +0530

    Update page.js

--- app/valentine/page.js
+++ app/valentine/page.js
@@ -1,25 +1,27 @@
-import Confetti from "../../components/Confetti";
-
 export default function Valentine() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "url('/images/valentine/valentine-bg.png') center/cover",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <img src="/images/valentine/kneeling-body.png" width={260} />
-      <img
-        src="/images/valentine/sameer-face.png"
-        width={90}
-        style={{ marginTop: -170 }}
-      />
-      <h1>Will you be my Valentine?</h1>
-      <Confetti />
-    </div>
+    <main style={page}>
+      <h1 style={title}>Will you be my Valentine?</h1>
+      <p style={sub}>There was never another ending.</p>
+    </main>
   );
 }
+
+/* ---------- styles ---------- */
+
+const page = {
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const title = {
+  fontSize: 38,
+  marginBottom: 18,
+};
+
+const sub = {
+  opacity: 0.6,
+};
