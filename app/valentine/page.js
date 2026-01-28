a3a34f876762365bee4058c29cf21e48f0cc4d5d
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-28 16:45:54 +0530

    Update page.js

--- app/valentine/page.js
+++ app/valentine/page.js
@@ -2,12 +2,23 @@ import Confetti from "../../components/Confetti";
 
 export default function Valentine() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-center">
-      <img src="/images/valentine/kneeling-body.png" className="w-64" />
-      <img src="/images/valentine/sameer-face.png" className="w-24 -mt-52" />
-      <h1 className="text-4xl mt-6 font-cinematic">
-        Will you be my Valentine?
-      </h1>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "url('/images/valentine/valentine-bg.png') center/cover",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img src="/images/valentine/kneeling-body.png" width={260} />
+      <img
+        src="/images/valentine/sameer-face.png"
+        width={90}
+        style={{ marginTop: -170 }}
+      />
+      <h1>Will you be my Valentine?</h1>
       <Confetti />
     </div>
   );
