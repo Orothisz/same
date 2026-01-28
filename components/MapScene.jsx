b928b863c432da63819748f5339b2c378430a83e
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-28 16:41:42 +0530

    Update MapScene.jsx

--- components/MapScene.jsx
+++ components/MapScene.jsx
@@ -1,3 +1,28 @@
+"use client";
+import MascotDialogue from "./MascotDialogue";
+
 export default function MapScene() {
-  return <div>Map Scene</div>;
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundImage: "url('/images/map/world-map-ghibli.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+          background:
+            "linear-gradient(to bottom, rgba(255,255,255,0.25), rgba(0,0,0,0.55))",
+        }}
+      />
+      <MascotDialogue />
+    </div>
+  );
 }
