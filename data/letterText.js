3905eed293f5c18e1cfae31cf881e2953fc398fb
Orothisz <112760300+Orothisz@users.noreply.github.com>
2026-01-28 20:33:20 +0530

    Update letterText.js

--- data/letterText.js
+++ data/letterText.js
@@ -1,8 +1,6 @@
 export const letterText = `
-This is where your long,
-personal,
-heartbreaking,
-beautiful letter goes.
-
-Replace this completely.
+lol hey so no paragraoh yet let's leave it for the actual day i love yeah so one thing
+i made this since i cant you know give you actual gifts nahi toh i planend that wait
+once u see the entire website ill tell then and haan okay so jab end me you get asked 
+a question try to press no alright loveyouu have fun
 `;
